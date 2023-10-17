0c87b84a5183b549c18b372ad8bd163999dacb6e
zijinChampion <xu.wang@thoughtworks.com>
2023-10-17 17:57:36 +0800

    feat: optimize create article

--- model/article.go
+++ model/article.go
@@ -1,5 +1,7 @@
 package model
 
+import "gorm.io/gorm"
+
 type Article struct {
 	Model
 
@@ -8,6 +10,22 @@ type Article struct {
 	AuthorId int    `gorm:"column:author_id" json:"article_author"`
 	TagId    int    `gorm:"column:tag_id" json:"tag_id"`
 	IsHidden bool   `gorm:"column:is_hidden" json:"is_hidden"`
+}
+
+type ArticleVO struct {
+	Id         int      `json:"id"`
+	Title      string   `json:"article_title"`
+	Content    string   `json:"article_content"`
+	AuthorName string   `json:"article_author"`
+	Tags       []string `json:"tags"`
+	IsHidden   bool     `json:"is_hidden"`
+}
+
+type ArticleTag struct {
+	Model
+
+	ArticleId int `gorm:"column:article_id"`
+	TagId     int `gorm:"column:tag_id"`
 }
 
 func GetAllArticles() (articles []Article, err error) {
@@ -25,14 +43,31 @@ func GetArticlesByAuthor(authorId int) (articles []Article, err error) {
 	return
 }
 
-func CreateNewArticle(title, content string, authorId, tagId int) (bool, error) {
-	err := client.Table("articles").Create(&Article{
-		Title:    title,
-		Content:  content,
-		AuthorId: authorId,
-		TagId:    tagId,
-		IsHidden: false,
-	}).Error
+func CreateNewArticle(title, content string, authorId int, tagIds []int) (bool, error) {
+
+	var err error
+
+	client.Transaction(func(tx *gorm.DB) error {
+
+		article := Article{
+			Title:    title,
+			Content:  content,
+			AuthorId: authorId,
+			IsHidden: false}
+
+		if err = client.Table("articles").Create(&article).Error; err != nil {
+			return err
+		}
+		for _, tagId := range tagIds {
+			if err = client.Table("article_tag").Create(&ArticleTag{
+				ArticleId: article.Id,
+				TagId:     tagId,
+			}).Error; err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 
 	return true, err
 }
